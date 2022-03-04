66992e2e6860f6e20dc8b994c93546e78ed6d5e7
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2022-03-04 20:33:48 +0200

    Add files via upload

---
+++ 5) Random Forest Regression/random-forest-regression.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("random-forest-regression-dataset.csv",sep = ";",header = None)
+
+x = df.iloc[:,0].values.reshape(-1,1)
+y = df.iloc[:,1].values.reshape(-1,1)
+
+#rondomForest
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor(n_estimators = 100, random_state = 42)
+rf.fit(x,y)
+
+x_ = np.arange(min(x),max(x),0.01).reshape(-1,1)
+y_head = rf.predict(x_)
+
+# visualize
+plt.scatter(x,y,color="red")
+plt.plot(x_,y_head,color="green")
+plt.xlabel("tribun level")
+plt.ylabel("ucret")
+plt.show()
