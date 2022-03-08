82cdde6ad28bacd2386c6e72efb5bf002c78df9c
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2022-03-08 20:12:31 +0200

    Add files via upload

---
+++ 11) Decision Tree Classification/decision-tree-tutorial.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+# import data
+data = pd.read_csv("data.csv")
+#sınıflandırma için işimize yaraman kısımları çıkarıyoruz 
+data.drop(["id","Unnamed: 32"],axis=1,inplace = True)
+
+# m değerlerini 1 ve b değerlerini 0 olarak değiştiriryoruz
+data.diagnosis = [ 1 if each == "M" else 0 for each in data.diagnosis]
+y = data.diagnosis.values
+x_data = data.drop(["diagnosis"],axis=1)
+
+# normalization
+x = (x_data - np.min(x_data))/(np.max(x_data)-np.min(x_data))
+
+#eğitim ve test datalarını oluşturuyoruz
+from sklearn.model_selection import train_test_split
+x_train, x_test,y_train, y_test = train_test_split(x,y,test_size = 0.15,random_state = 42)
+
+
+from sklearn.tree import DecisionTreeClassifier
+dt = DecisionTreeClassifier()
+dt.fit(x_train,y_train)
+
+print("score: ", dt.score(x_test,y_test))
+
